18a92b8231482e5a3464448b1b85fc8db988b1e6
AndreaDiC11 <andreadicarlo10@gmail.com>
2023-03-28 23:01:35 +0200

    Add Tank Movement

--- Source/ToonTanks/BasePawn.cpp
+++ Source/ToonTanks/BasePawn.cpp
@@ -2,12 +2,26 @@
 
 
 #include "BasePawn.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/StaticMeshComponent.h"
 
 // Sets default values
 ABasePawn::ABasePawn()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collider"));
+	RootComponent = CapsuleComp;
+
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base Mesh"));
+	BaseMesh->SetupAttachment(CapsuleComp);
+
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Turret Mesh"));
+	TurretMesh->SetupAttachment(BaseMesh);
+
+	ProjectileSpawnPoint = CreateDefaultSubobject<USceneComponent>(TEXT("Spawn Point"));
+	ProjectileSpawnPoint->SetupAttachment(TurretMesh);
 
 }
 
@@ -23,12 +37,8 @@ void ABasePawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-}
-
-// Called to bind functionality to input
-void ABasePawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 }
+
+
 
--- Source/ToonTanks/BasePawn.h
+++ Source/ToonTanks/BasePawn.h
@@ -3,7 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Pawn.h"
+#include "GameFramework/Pawn.h" 
 #include "BasePawn.generated.h"
 
 UCLASS()
@@ -19,11 +19,19 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+private:
+	UPROPERTY(VisibleAnywhere,  BlueprintReadOnly, Category =  "Components", meta = (AllowPrivateAccess = "true") )
+	class UCapsuleComponent* CapsuleComp;
+	UPROPERTY(VisibleAnywhere,  BlueprintReadOnly, Category =  "Components", meta = (AllowPrivateAccess = "true") )
+	UStaticMeshComponent* BaseMesh;
+	UPROPERTY(VisibleAnywhere,  BlueprintReadOnly, Category =  "Components", meta = (AllowPrivateAccess = "true") )
+	UStaticMeshComponent* TurretMesh;
+	UPROPERTY(VisibleAnywhere,  BlueprintReadOnly, Category =  "Components", meta = (AllowPrivateAccess = "true") )
+	USceneComponent* ProjectileSpawnPoint;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 };
